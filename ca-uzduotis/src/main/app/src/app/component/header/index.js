0eda92e59463e3bc064a6075910adbc051832a33
Jassius <jonas.rinkevicius@gmail.com>
2019-07-01 19:16:32 +0300

    veikia dalinis vaikščiojimas po meniu

--- ca-uzduotis/src/main/app/src/app/component/header/index.js
+++ ca-uzduotis/src/main/app/src/app/component/header/index.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 import { Jumbotron, Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
 import LoginModal from '../modal/login';
-import AddArticle from '../modal/addArticle';
+import AddArticle from '../addArticle';
 
 
 
-function Header() {
-    return (
-        <header className={Jumbotron}>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">Kshysia blog</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <LoginModal />
-                        <AddArticle />
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
-    );
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleshowAddArticle = props.handleshowAddArticle.bind(this);
+        this.state = {
+            showAddArticle: props.showAddArticle,
+            showArticleList: props.showArticleList,
+            showArticle: props.showArticle,
+        };
+
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.handleshowAddArticle()}>Add Article</button></div >
+
+            // <header className={Jumbotron}>
+
+            //     <Navbar bg="light" expand="lg">
+            //         <Navbar.Brand href="#home">Kshysia blog</Navbar.Brand>
+            //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            //         <Navbar.Collapse id="basic-navbar-nav">
+            //             <Nav className="mr-auto">
+            //                 <LoginModal />
+
+            //             </Nav>
+            //         </Navbar.Collapse>
+            //     </Navbar>
+            // </header >
+        );
+    }
 }
 
 export default Header;
--- ca-uzduotis/src/main/app/src/app/index.js
+++ ca-uzduotis/src/main/app/src/app/index.js
@@ -4,21 +4,50 @@ import Header from './component/header';
 import { Container, Button } from 'react-bootstrap';
 import Article from './component/article';
 import ArticleList from './component/articleList';
+import AddArticle from './component/addArticle';
 
 
-function Index() {
-    return (
-        <Container>
-            <div className="App">
-                <Header />
-                <main className="App--content ">
-                    <ArticleList />
-                    <Article />
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleshowAddArticle = this.handleshowAddArticle.bind(this);
+        this.handleArticleListClick = this.handleArticleListClick.bind(this);
+        this.state = {
+            showAddArticle: false,
+            showArticleList: true,
+            showArticle: false,
+        };
 
-                </main>
-            </div>
-        </Container>
-    );
+    }
+    handleshowAddArticle(props) {
+        this.setState({
+            showAddArticle: !this.state.showAddArticle,
+            showArticleList: !this.state.showArticleList,
+            showArticle: !this.state.showArticle,
+        });
+
+    }
+    handleArticleListClick() {
+        this.setState({
+            showAddArticle: false,
+            showArticleList: false,
+            showArticle: true,
+        });
+    }
+    render() {
+        return (
+            <Container>
+                <div className="App">
+                    <Header handleshowAddArticle={this.handleshowAddArticle} />
+                    <main className="App--content ">
+                        {this.state.showArticleList ? <ArticleList handleArticleListClick={this.handleArticleListClick} /> : null}
+                        {this.state.showAddArticle ? <AddArticle /> : null}
+                        {this.state.showArticle ? <Article /> : null}
+                    </main>
+                </div>
+            </Container>
+        );
+    }
 }
 
 export default Index;
